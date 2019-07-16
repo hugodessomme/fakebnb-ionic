550941debfdc24cd588f907bc624457c27a876ca
hugodessomme <hdessomme@gmail.com>
2019-07-16 15:08:53 +0200

    Add side menu

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { PlacesService } from "../places.service";
 import { Place } from "../place.model";
+import { MenuController } from "@ionic/angular";
+import { timingSafeEqual } from "crypto";
 
 @Component({
   selector: "app-discover",
@@ -10,9 +12,16 @@ import { Place } from "../place.model";
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
 
-  constructor(private placesService: PlacesService) {}
+  constructor(
+    private placesService: PlacesService,
+    private menuController: MenuController
+  ) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
   }
+
+  // onOpenMenu() {
+  //   this.menuController.open();
+  // }
 }
