a713f5067b3f7b0a94ec5bd8c9829778c1ed158b
hugodessomme <hdessomme@gmail.com>
2019-07-16 14:51:37 +0200

    Finish Edit & PlaceDetail pages

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 import { NavController } from "@ionic/angular";
+import { PlacesService } from "../../places.service";
+import { Place } from "../../place.model";
 
 @Component({
   selector: "app-place-detail",
@@ -8,9 +10,25 @@ import { NavController } from "@ionic/angular";
   styleUrls: ["./place-detail.page.scss"]
 })
 export class PlaceDetailPage implements OnInit {
-  constructor(private router: Router, private navController: NavController) {}
+  place: Place;
 
-  ngOnInit() {}
+  constructor(
+    private router: Router,
+    private navController: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has("placeId")) {
+        this.navController.navigateBack("/places/tabs/discover");
+        return;
+      }
+
+      this.place = this.placesService.getPlace(paramMap.get("placeId"));
+    });
+  }
 
   onBookPlace() {
     // this.router.navigateByUrl("/places/tabs/discover");
